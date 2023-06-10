af5c3877b9e1bf8d7f3622e1836e6515b93bd2ad
luis2889 <marquezweffer@gmail.com>
2023-06-10 12:35:27 -0400

    finalizacion de navbar

---
+++ header.php/index.php
@@ -0,0 +1,27 @@
+<?php
+include("includes/header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Task page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <i class="bi bi-bell-fill"></i>
+          To Do Page
+        </a>
+      </div>
+    </nav>
+  </header>
+
+  <main>
