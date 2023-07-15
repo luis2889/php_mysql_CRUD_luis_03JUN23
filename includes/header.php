01cecc0bb54832084bbaf7252eca83edbac7d893
luis2889 <marquezweffer@gmail.com>
2023-07-15 10:01:18 -0400

    image.php

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Task page</title>
+<title>Task page</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+  <link href="style.css" rel="stylesheet">
 </head>
 <body>
   <header>
@@ -15,9 +10,10 @@
         <a class="navbar-brand" href="#">
           <i class="bi bi-bell-fill"></i>
           Página de tareas
+          Página de taréas
         </a>
       </div>
     </nav>
   </header>
-
  
+  </header>
--- index.php
+++ index.php
@@ -6,41 +6,47 @@
   include("includes/header.php");
 ?>
 
-<main class="container p-4">
-  <div class="row">
-    <div class="col-md-4">
-    <!-- MESSAGES -->
-
-      <?php if(isset($_SESSION['message'])) { ?>
-      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
-        <?= $_SESSION['message']?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      <?php session_unset(); } ?>
-
-      <!-- ADD TASK FORM -->
-      <div class="card card-body">
-        <form action="save_task.php" method="POST" enctype="multipart/form-data">
-          <div class="form-group mb-3">
-            <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
-          </div>
-          <div class="form-group mb-3">
-            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
-          </div>
-          <div class="form-group mb-3">
-            <input type="file" name="image" class="form-control">
-          </div>
-          <input type="submit" name="save_task" class="btn btn-success btn-block w-100" value="Save Task">
-        </form>
-      </div>
+@@ -36,7 +33,43 @@
     </div>
 
     <div class="col-md-8">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created At</th>
+            <th>Image</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
 
+        <?php
+          $query = "SELECT * FROM task";
+          $result_tasks = mysqli_query($conn, $query);
+
+          while($row = mysqli_fetch_assoc($result_tasks)) {
+          ?>
+          <tr>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['description']; ?></td>
+            <td><?php echo $row['created_at']; ?></td>
+            <td class="resize-image">
+              <img src="image.php?id=<?php echo $row['id'] ?>" alt="image" class="w-100">
+            </td>
+            <td>
+              <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary rounded-circle">
+                <ion-icon name="create"></ion-icon>
+              </a>
+              <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                <ion-icon name="trash"></ion-icon>
+              </a>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
     </div>
   </div>
 </main>
-
-<?php
-  include("includes/footer.php");
-?>
